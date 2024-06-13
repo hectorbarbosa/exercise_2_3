aabcdfeb8ea6ce471c8e1813c45fb59ac23f3b6e
Eugene Kvinto <ekvinto@yandex.ru>
2024-06-13 18:06:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+
+app.listen(PORT, () =>
+  console.log(`server listening on port ${PORT}...`)
+);
+
+module.exports = app;
